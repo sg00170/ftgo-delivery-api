4ee77b1c6cd968010d8f64ffabe3a4e2a9d881d0
Misun Kwon <sg00170@gmail.com>
2025-05-13 18:53:45 +0900

    feature(main): 주문 테이블 추가

---
+++ src/main/java/com/sg00170/delivery/model/Order.java
@@ -0,0 +1,19 @@
+package com.sg00170.delivery.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "orders")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private short state;
+}
